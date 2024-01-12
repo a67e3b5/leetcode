f214350014ac546bc6438a20f9fd0a214aa7758d
a67e3b5 <142142760+a67e3b5@users.noreply.github.com>
2024-01-12 23:56:28 +0900

    1768
    Signed-off-by: a67e3b5 <142142760+a67e3b5@users.noreply.github.com>

--- src/_1768_merge_strings_alternately.rs
+++ src/_1768_merge_strings_alternately.rs
@@ -7,7 +7,26 @@
 // @lc code=start
 impl Solution {
     pub fn merge_alternately(word1: String, word2: String) -> String {
-        String::new()
+        let mut word1_rev = word1.chars().rev().collect::<String>();
+        let mut word2_rev = word2.chars().rev().collect::<String>();
+        let mut res = String::new();
+        loop {
+            if let Some(c) = word1_rev.pop() {
+                res.push(c)
+            } else {
+                if word2_rev.is_empty() {
+                    break;
+                }
+            }
+            if let Some(c) = word2_rev.pop() {
+                res.push(c)
+            } else {
+                if word1_rev.is_empty() {
+                    break;
+                }
+            }
+        }
+        res
     }
 }
 // @lc code=end
