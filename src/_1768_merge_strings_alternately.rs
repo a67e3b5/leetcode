6a67f40021a687b08c262baee819e47dc5676759
a67e3b5 <142142760+a67e3b5@users.noreply.github.com>
2024-01-12 23:12:42 +0900

    Initialize to use `vscode-leetcode` and `rust-analyzer`
    Signed-off-by: a67e3b5 <142142760+a67e3b5@users.noreply.github.com>

---
+++ src/_1768_merge_strings_alternately.rs
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1768 lang=rust
+ *
+ * [1768] Merge Strings Alternately
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn merge_alternately(word1: String, word2: String) -> String {
+        String::new()
+    }
+}
+// @lc code=end
+
+struct Solution;
