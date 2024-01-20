648800f73f86abc3e113705e140c0aaa192dfb5a
a67e3b5 <142142760+a67e3b5@users.noreply.github.com>
2024-01-20 13:03:38 +0900

    933-5
    Signed-off-by: a67e3b5 <142142760+a67e3b5@users.noreply.github.com>

--- src/_933_number_of_recent_calls.rs
+++ src/_933_number_of_recent_calls.rs
@@ -6,25 +6,23 @@
 
 // @lc code=start
 struct RecentCounter {
-    lap_times: VecDeque<i32>,
+    lap_times: Vec<i32>,
 }
 
 /**
  * `&self` means the method takes an immutable reference.
  * If you need a mutable reference, change it to `&mut self` instead.
  */
-use std::collections::VecDeque;
-
 impl RecentCounter {
     fn new() -> Self {
         Self {
-            lap_times: VecDeque::new(),
+            lap_times: Vec::new(),
         }
     }
 
     fn ping(&mut self, t: i32) -> i32 {
         let since = self.lap_times.partition_point(|&s| s < t - 3000);
-        self.lap_times.push_back(t);
+        self.lap_times.push(t);
         self.lap_times.len() as i32 - since as i32
     }
 }
