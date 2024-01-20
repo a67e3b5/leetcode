d9699d87429fb666269ef2605c4b93b0de9dba9e
a67e3b5 <142142760+a67e3b5@users.noreply.github.com>
2024-01-20 20:36:41 +0900

    2215-2r
    Signed-off-by: a67e3b5 <142142760+a67e3b5@users.noreply.github.com>

--- src/_2215_find_the_difference_of_two_arrays.rs
+++ src/_2215_find_the_difference_of_two_arrays.rs
@@ -7,12 +7,18 @@
 // @lc code=start
 impl Solution {
     pub fn find_difference(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<Vec<i32>> {
-        use std::collections::HashSet;
-        let nums1 = nums1.into_iter().collect::<HashSet<_>>();
-        let nums2 = nums2.into_iter().collect::<HashSet<_>>();
-        let diff12 = nums1.difference(&nums2).cloned().collect::<Vec<_>>();
-        let diff21 = nums2.difference(&nums1).cloned().collect::<Vec<_>>();
-        vec![diff12, diff21]
+        let idx = |x: i32| -> usize { (x + 1000) as _ };
+        let mut counter = [0_u8; 2001];
+        nums1.iter().for_each(|&x| counter[idx(x)] |= 0b01);
+        nums2.iter().for_each(|&x| counter[idx(x)] |= 0b10);
+
+        let mut res = vec![Vec::new(), Vec::new()];
+        counter.iter().enumerate().for_each(|(i, &x)| match x {
+            0b01 => res[0].push(i as i32 - 1000),
+            0b10 => res[1].push(i as i32 - 1000),
+            _ => (),
+        });
+        res
     }
 }
 // @lc code=end
