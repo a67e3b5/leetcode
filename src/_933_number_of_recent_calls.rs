1ddbaf7fc19233d627ba1e61425a6bc97edff957
a67e3b5 <142142760+a67e3b5@users.noreply.github.com>
2024-01-20 13:08:06 +0900

    Revert "933-5"
    This reverts commit 648800f73f86abc3e113705e140c0aaa192dfb5a.

--- src/_933_number_of_recent_calls.rs
+++ src/_933_number_of_recent_calls.rs
@@ -6,23 +6,25 @@
 
 // @lc code=start
 struct RecentCounter {
-    lap_times: Vec<i32>,
+    lap_times: VecDeque<i32>,
 }
 
 /**
  * `&self` means the method takes an immutable reference.
  * If you need a mutable reference, change it to `&mut self` instead.
  */
+use std::collections::VecDeque;
+
 impl RecentCounter {
     fn new() -> Self {
         Self {
-            lap_times: Vec::new(),
+            lap_times: VecDeque::new(),
         }
     }
 
     fn ping(&mut self, t: i32) -> i32 {
         let since = self.lap_times.partition_point(|&s| s < t - 3000);
-        self.lap_times.push(t);
+        self.lap_times.push_back(t);
         self.lap_times.len() as i32 - since as i32
     }
 }
