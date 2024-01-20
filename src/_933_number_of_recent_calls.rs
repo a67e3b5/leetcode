96dcdf8f524007abdaf20c4ce8909317b31c3c93
a67e3b5 <142142760+a67e3b5@users.noreply.github.com>
2024-01-20 13:25:42 +0900

    933-9r
    Signed-off-by: a67e3b5 <142142760+a67e3b5@users.noreply.github.com>

--- src/_933_number_of_recent_calls.rs
+++ src/_933_number_of_recent_calls.rs
@@ -23,13 +23,10 @@ impl RecentCounter {
     }
 
     fn ping(&mut self, t: i32) -> i32 {
-        while let Some(&s) = self.lap_times.front() {
-            if t - 3000 <= s {
-                break;
-            }
+        self.lap_times.push_back(t);
+        while self.lap_times[0] < t - 3000 {
             self.lap_times.pop_front();
         }
-        self.lap_times.push_back(t);
         self.lap_times.len() as i32
     }
 }
