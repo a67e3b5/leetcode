cb27334a92e7f6b6648948f237efe66ba594ed79
a67e3b5 <142142760+a67e3b5@users.noreply.github.com>
2024-01-25 01:49:26 +0900

    334-6
    Signed-off-by: a67e3b5 <142142760+a67e3b5@users.noreply.github.com>

--- src/_334_increasing_triplet_subsequence.rs
+++ src/_334_increasing_triplet_subsequence.rs
@@ -8,9 +8,7 @@
 impl Solution {
     pub fn increasing_triplet(nums: Vec<i32>) -> bool {
         let (mut n0, mut n1) = (nums[0], i32::MAX);
-        #[allow(clippy::needless_range_loop)]
-        for i in 0..nums.len() {
-            let num = nums[i];
+        for num in nums {
             if n1 < num {
                 return true;
             }
