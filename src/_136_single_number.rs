486b1de1e6c00285dbe87eff49f346743a128fbb
a67e3b5 <142142760+a67e3b5@users.noreply.github.com>
2024-01-24 17:12:05 +0900

    136-2r
    Signed-off-by: a67e3b5 <142142760+a67e3b5@users.noreply.github.com>

--- src/_136_single_number.rs
+++ src/_136_single_number.rs
@@ -7,7 +7,7 @@
 // @lc code=start
 impl Solution {
     pub fn single_number(nums: Vec<i32>) -> i32 {
-        nums.iter().fold(0, |acc, &x| acc ^ x)
+        nums.into_iter().fold(0, |acc, x| acc ^ x)
     }
 }
 // @lc code=end
