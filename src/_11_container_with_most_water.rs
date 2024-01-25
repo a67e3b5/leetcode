c46efadf7bf97065b122550dbb243eb40684136a
a67e3b5 <142142760+a67e3b5@users.noreply.github.com>
2024-01-25 19:12:37 +0900

    11-4: TLE
    Signed-off-by: a67e3b5 <142142760+a67e3b5@users.noreply.github.com>

--- src/_11_container_with_most_water.rs
+++ src/_11_container_with_most_water.rs
@@ -5,30 +5,38 @@
  */
 
 // @lc code=start
-use std::cmp::{max, min};
+use std::collections::VecDeque;
 
 impl Solution {
     pub fn max_area(height: Vec<i32>) -> i32 {
-        let height = height.into_iter().map(|h| h as usize).collect::<Vec<_>>();
-        let area = |l_idx: usize, r_idx: usize| (r_idx - l_idx) * min(height[l_idx], height[r_idx]);
-        let (mut l_idx, mut r_idx) = (0_usize, height.len() - 1);
-        let mut ans = area(l_idx, r_idx);
-        while l_idx < r_idx {
-            let area_l_shrink = area(l_idx + 1, r_idx);
-            let area_r_shrink = area(l_idx, r_idx - 1);
-            dbg!(&l_idx, &r_idx, &ans, &area_l_shrink, &area_r_shrink);
-            if ans < max(area_l_shrink, area_r_shrink) {
-                if area_l_shrink < area_r_shrink {
-                    ans = area_r_shrink;
-                    r_idx -= 1;
-                } else {
-                    ans = area_l_shrink;
-                    l_idx += 1;
-                }
+        let mut height = height
+            .into_iter()
+            .map(|i| i as usize)
+            .collect::<VecDeque<_>>();
+        let mut ans = 0;
+        let mut max_height = 0;
+        while let Some(&h_l) = height.get(0) {
+            if h_l <= max_height {
+                height.pop_front();
+                continue; // optimization
             } else {
-                l_idx += 1;
-                r_idx -= 1;
+                max_height = h_l;
             }
+            let skip = ans / h_l + 1;
+            let Some(max_area_in_this_l) = height
+                .iter()
+                .enumerate()
+                .skip(skip) // optimization
+                .map(|(i, &h_r)| i * h_l.min(h_r))
+                .max()
+            else {
+                height.pop_front();
+                continue;
+            };
+            if ans < max_area_in_this_l {
+                ans = max_area_in_this_l;
+            }
+            height.pop_front();
         }
         ans as i32
     }
@@ -43,7 +51,10 @@ mod tests {
 
     #[test]
     fn test() {
-        let samples = [(vec![2, 3, 10, 5, 7, 8, 9], 36)];
+        let samples = [
+            (vec![2, 3, 10, 5, 7, 8, 9], 36),
+            (vec![1, 3, 2, 5, 25, 24, 5], 24),
+        ];
         for (input, output) in samples {
             assert_eq!(Solution::max_area(input), output);
         }
