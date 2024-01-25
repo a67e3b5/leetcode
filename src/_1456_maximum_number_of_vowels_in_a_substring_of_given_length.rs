6c78deb891c33ff92126efa9e4c1fcaa404d8377
a67e3b5 <142142760+a67e3b5@users.noreply.github.com>
2024-01-26 01:06:51 +0900

    1456-2
    Signed-off-by: a67e3b5 <142142760+a67e3b5@users.noreply.github.com>

--- src/_1456_maximum_number_of_vowels_in_a_substring_of_given_length.rs
+++ src/_1456_maximum_number_of_vowels_in_a_substring_of_given_length.rs
@@ -7,14 +7,42 @@
 // @lc code=start
 impl Solution {
     pub fn max_vowels(s: String, k: i32) -> i32 {
-        let s = s.into_bytes();
+        let k = k as usize;
         let is_vowel = |b: &u8| matches!(b, b'a' | b'e' | b'i' | b'o' | b'u');
-        s.windows(k as usize)
-            .map(|slice| slice.iter().filter(|&b| is_vowel(b)).count())
-            .max()
-            .unwrap() as i32
+        let booleans = s.into_bytes().iter().map(is_vowel).collect::<Vec<_>>();
+        let mut num_vowels = booleans.iter().take(k).filter(|&&b| b).count();
+        let mut ans = num_vowels;
+        let mut i = 0;
+        while i + k < booleans.len() {
+            let (excluding, including) = (booleans[i], booleans[i + k]);
+            if excluding ^ including {
+                if excluding {
+                    num_vowels -= 1;
+                } else {
+                    num_vowels += 1;
+                    if ans < num_vowels {
+                        ans = num_vowels
+                    }
+                }
+            }
+            i += 1;
+        }
+        ans as i32
     }
 }
 // @lc code=end
 
 struct Solution;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let samples = [(("abciiidef", 3), 3)];
+        for (input, output) in samples {
+            assert_eq!(Solution::max_vowels(input.0.to_string(), input.1), output);
+        }
+    }
+}
