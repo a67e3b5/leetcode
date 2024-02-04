9bcee6bd4c1072912e92cb24e63b652520ccb9cf
a67e3b5 <142142760+a67e3b5@users.noreply.github.com>
2024-02-04 23:30:00 +0900

    1926-14r: MLE
    Signed-off-by: a67e3b5 <142142760+a67e3b5@users.noreply.github.com>

--- src/_1926_nearest_exit_from_entrance_in_maze.rs
+++ src/_1926_nearest_exit_from_entrance_in_maze.rs
@@ -8,70 +8,59 @@
 impl Solution {
     pub fn nearest_exit(mut maze: Vec<Vec<char>>, entrance: Vec<i32>) -> i32 {
         const WALL: char = '+';
-        const FRONT: char = '?';
         let entrance = (entrance[0] as usize, entrance[1] as usize);
         let m_rows = maze.len();
         let n_cols = maze[0].len();
         let is_wall = |c: char| c == WALL;
-        let is_front = |c: char| c == FRONT;
-        let is_exit =
-            |p: (usize, usize)| p.0 == 0 || p.0 == m_rows - 1 || p.1 == 0 || p.1 == n_cols - 1;
+        let on_edge =
+            |(i, j): (usize, usize)| i == 0 || i == m_rows - 1 || j == 0 || j == n_cols - 1;
         let mut steps = 0;
+        let mut front = vec![entrance];
 
-        maze[entrance.0][entrance.1] = FRONT;
-
-        if is_exit(entrance) {
-            let (row, col) = entrance;
-            if row != 0 && !is_wall(maze[row - 1][col]) {
-                maze[row - 1][col] = FRONT;
+        if on_edge(entrance) {
+            let (i, j) = entrance;
+            front.pop();
+            if i != 0 && !is_wall(maze[i - 1][j]) {
+                front.push((i - 1, j));
             }
-            if row != m_rows - 1 && !is_wall(maze[row + 1][col]) {
-                maze[row + 1][col] = FRONT;
+            if i != m_rows - 1 && !is_wall(maze[i + 1][j]) {
+                front.push((i + 1, j));
             }
-            if col != 0 && !is_wall(maze[row][col - 1]) {
-                maze[row][col - 1] = FRONT;
+            if j != 0 && !is_wall(maze[i][j - 1]) {
+                front.push((i, j - 1));
             }
-            if col != n_cols - 1 && !is_wall(maze[row][col + 1]) {
-                maze[row][col + 1] = FRONT;
+            if j != n_cols - 1 && !is_wall(maze[i][j + 1]) {
+                front.push((i, j + 1));
             }
-            maze[row][col] = WALL;
+            maze[i][j] = WALL;
             steps += 1;
         }
 
-        loop {
+        while !front.is_empty() {
             let mut next = vec![];
-            for row in 0..m_rows {
-                for col in 0..n_cols {
-                    if !is_front(maze[row][col]) {
-                        continue;
-                    }
-                    if is_exit((row, col)) {
-                        return steps;
-                    }
-                    if !is_wall(maze[row - 1][col]) {
-                        next.push((row - 1, col))
-                    }
-                    if !is_wall(maze[row + 1][col]) {
-                        next.push((row + 1, col))
-                    }
-                    if !is_wall(maze[row][col - 1]) {
-                        next.push((row, col - 1))
-                    }
-                    if !is_wall(maze[row][col + 1]) {
-                        next.push((row, col + 1))
-                    }
-                    // Bury with a wall the current location so as not to be revisited.
-                    maze[row][col] = WALL;
+            for (i, j) in front {
+                if on_edge((i, j)) {
+                    return steps;
                 }
+                if !is_wall(maze[i - 1][j]) {
+                    next.push((i - 1, j))
+                }
+                if !is_wall(maze[i + 1][j]) {
+                    next.push((i + 1, j))
+                }
+                if !is_wall(maze[i][j - 1]) {
+                    next.push((i, j - 1))
+                }
+                if !is_wall(maze[i][j + 1]) {
+                    next.push((i, j + 1))
+                }
+                // Bury with a wall the current location so as not to be revisited.
+                maze[i][j] = WALL;
             }
-            if next.is_empty() {
-                return -1;
-            }
-            for (row, col) in next {
-                maze[row][col] = FRONT;
-            }
+            front = next;
             steps += 1;
         }
+        -1
     }
 }
 // @lc code=end
