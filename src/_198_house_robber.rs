fba329d940eab44ffc4f29f64f955bcf597d2ade
a67e3b5 <142142760+a67e3b5@users.noreply.github.com>
2024-02-07 08:06:13 +0900

    198-2r
    Signed-off-by: a67e3b5 <142142760+a67e3b5@users.noreply.github.com>

--- src/_198_house_robber.rs
+++ src/_198_house_robber.rs
@@ -5,22 +5,11 @@
  */
 
 // @lc code=start
-// This is the dual of #746
 impl Solution {
-    pub fn rob(loss: Vec<i32>) -> i32 {
-        let mut street_end = loss.len();
-        // Loss to the end from the end
-        let mut l0 = 0;
-        // Loss to the end from 1 step before the end
-        let mut l1 = loss[street_end - 1];
-
-        while 1 < street_end {
-            let l2 = loss[street_end - 2] + l1.min(l0);
-            street_end -= 1;
-            l0 = l1;
-            l1 = l2;
-        }
-        loss.into_iter().sum::<i32>() - l1.min(l0)
+    pub fn rob(nums: Vec<i32>) -> i32 {
+        nums.into_iter()
+            .fold((0, 0), |(sum2, sum1), num0| (sum1, sum1.max(sum2 + num0)))
+            .1
     }
 }
 // @lc code=end
