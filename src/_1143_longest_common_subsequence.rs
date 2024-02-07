55bdde589a96290e5251d5867aedba31f3cb0189
a67e3b5 <142142760+a67e3b5@users.noreply.github.com>
2024-02-08 00:11:33 +0900

    1143-6r
    Signed-off-by: a67e3b5 <142142760+a67e3b5@users.noreply.github.com>

--- src/_1143_longest_common_subsequence.rs
+++ src/_1143_longest_common_subsequence.rs
@@ -9,37 +9,17 @@ impl Solution {
     pub fn longest_common_subsequence(text1: String, text2: String) -> i32 {
         let m = text1.len();
         let n = text2.len();
-        let mut match_grid = vec![vec![false; n]; m];
+        let mut grid = vec![vec![0; n + 1]; m + 1];
         for (i, c1) in text1.char_indices() {
             for (j, c2) in text2.char_indices() {
-                if c1 == c2 {
-                    match_grid[i][j] = true;
-                }
+                grid[i + 1][j + 1] = if c1 == c2 {
+                    grid[i][j] + 1
+                } else {
+                    grid[i][j + 1].max(grid[i + 1][j])
+                };
             }
         }
-        let mut sequence_grid = vec![vec![vec![]; n]; m];
-        for i in 0..m {
-            for j in 0..n {
-                let mut u = if i == 0 {
-                    vec![]
-                } else {
-                    sequence_grid[i - 1][j].clone()
-                };
-                if match_grid[i][j] && !u.iter().any(|p: &(usize, usize)| p.0 == i || p.1 == j) {
-                    u.push((i, j));
-                }
-                let mut l = if j == 0 {
-                    vec![]
-                } else {
-                    sequence_grid[i][j - 1].clone()
-                };
-                if match_grid[i][j] && !l.iter().any(|p: &(usize, usize)| p.0 == i || p.1 == j) {
-                    l.push((i, j));
-                }
-                sequence_grid[i][j] = std::cmp::max_by_key(u, l, |v| v.len());
-            }
-        }
-        sequence_grid[m - 1][n - 1].len() as i32
+        grid[m][n] as i32
     }
 }
 // @lc code=end
