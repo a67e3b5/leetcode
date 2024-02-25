06dc1b6d719d6b2b7440b4e4e9822df3bebfc1ed
a67e3b5 <142142760+a67e3b5@users.noreply.github.com>
2024-02-25 10:20:08 +0900

    2542-2: TLE
    Signed-off-by: a67e3b5 <142142760+a67e3b5@users.noreply.github.com>

--- src/_2542_maximum_subsequence_score.rs
+++ src/_2542_maximum_subsequence_score.rs
@@ -10,16 +10,21 @@ impl Solution {
         let k = k as usize;
         let mut zip = nums1.into_iter().zip(nums2).collect::<Vec<_>>();
         zip.sort_unstable_by_key(|(_, n2)| *n2);
+        let mut v1 = zip
+            .iter()
+            .enumerate()
+            .map(|(i, p)| (p.0, i))
+            .collect::<Vec<_>>();
+        v1.sort_unstable_by_key(|(n1, _)| -*n1);
         let mut ans = 0;
-        let n = zip.len();
-        for (i, (n1, n2)) in zip.iter().enumerate().take(n - k + 1) {
-            let mut v1 = zip
-                .iter()
-                .skip(i + 1)
-                .map(|(n1, _)| *n1 as i64)
-                .collect::<Vec<_>>();
-            v1.sort_unstable();
-            ans = ans.max((*n1 as i64 + v1.iter().skip(n - k - i).sum::<i64>()) * *n2 as i64)
+        for (i, (n1, n2)) in zip.iter().enumerate().take(zip.len() - k + 1) {
+            let sum = *n1 as i64
+                + v1.iter()
+                    .filter(|(_, j)| i < *j)
+                    .take(k - 1)
+                    .map(|(n, _)| *n as i64)
+                    .sum::<i64>();
+            ans = ans.max(sum * *n2 as i64)
         }
         ans
     }
