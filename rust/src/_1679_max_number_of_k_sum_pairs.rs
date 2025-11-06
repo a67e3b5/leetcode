59b40b4711c05b1561fcb7505b94a99637584900
a67e3b5 <142142760+a67e3b5@users.noreply.github.com>
2025-11-06 22:11:37 +0900

    1679-3r
    Signed-off-by: a67e3b5 <142142760+a67e3b5@users.noreply.github.com>

--- rust/src/_1679_max_number_of_k_sum_pairs.rs
+++ rust/src/_1679_max_number_of_k_sum_pairs.rs
@@ -4,26 +4,30 @@
  * [1679] Max Number of K-Sum Pairs
  */
 
-use std::usize;
-
 // @lc code=start
+use std::collections::HashMap;
+
 impl Solution {
-    pub fn max_operations(mut nums: Vec<i32>, k: i32) -> i32 {
-        let mut count = 0;
-        while let Some(end) = nums.pop() {
-            let mut index_remove = usize::MAX;
-            for (i, num) in nums.iter().enumerate() {
-                if num + end == k {
-                    index_remove = i;
-                    break;
+    pub fn max_operations(nums: Vec<i32>, k: i32) -> i32 {
+        let mut res = 0;
+        let mut hashmap = HashMap::new();
+        for num in nums {
+            let target = k - num;
+            match hashmap.get_mut(&target) {
+                Some(a) => {
+                    if *a > 1 {
+                        *a -= 1;
+                    } else {
+                        hashmap.remove_entry(&target);
+                    }
+                    res += 1;
+                }
+                None => {
+                    *hashmap.entry(num).or_insert(0) += 1;
                 }
             }
-            if index_remove != usize::MAX {
-                nums.remove(index_remove);
-                count += 1;
-            }
         }
-        count
+        res
     }
 }
 // @lc code=end
