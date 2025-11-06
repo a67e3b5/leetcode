d70f0d7a96166569b1db5ffbb8887afe26989eee
a67e3b5 <142142760+a67e3b5@users.noreply.github.com>
2025-11-06 22:02:03 +0900

    1679-2: TLE
    Signed-off-by: a67e3b5 <142142760+a67e3b5@users.noreply.github.com>

--- rust/src/_1679_max_number_of_k_sum_pairs.rs
+++ rust/src/_1679_max_number_of_k_sum_pairs.rs
@@ -4,29 +4,24 @@
  * [1679] Max Number of K-Sum Pairs
  */
 
+use std::usize;
+
 // @lc code=start
 impl Solution {
-    pub fn max_operations(nums: Vec<i32>, k: i32) -> i32 {
-        let mut indices: Vec<usize> = (0..nums.len()).collect();
+    pub fn max_operations(mut nums: Vec<i32>, k: i32) -> i32 {
         let mut count = 0;
-        loop {
-            let mut remove = vec![];
-            'a: for (i, ii) in indices.iter().enumerate() {
-                for (j, jj) in indices.iter().enumerate().skip_while(|(j, _)| *j <= i) {
-                    if nums[*ii] + nums[*jj] == k {
-                        remove.push(j);
-                        remove.push(i);
-                        break 'a;
-                    }
+        while let Some(end) = nums.pop() {
+            let mut index_remove = usize::MAX;
+            for (i, num) in nums.iter().enumerate() {
+                if num + end == k {
+                    index_remove = i;
+                    break;
                 }
             }
-            if remove.is_empty() {
-                break;
+            if index_remove != usize::MAX {
+                nums.remove(index_remove);
+                count += 1;
             }
-            for i in remove {
-                indices.remove(i);
-            }
-            count += 1;
         }
         count
     }
