2033333615d6bc0ce6169f81eaaef4a6bc0d6919
a67e3b5 <142142760+a67e3b5@users.noreply.github.com>
2025-11-06 20:00:44 +0900

    714-3r
    Signed-off-by: a67e3b5 <142142760+a67e3b5@users.noreply.github.com>

--- rust/src/_714_best_time_to_buy_and_sell_stock_with_transaction_fee.rs
+++ rust/src/_714_best_time_to_buy_and_sell_stock_with_transaction_fee.rs
@@ -8,23 +8,14 @@
 impl Solution {
     pub fn max_profit(prices: Vec<i32>, fee: i32) -> i32 {
         let len = prices.len();
-        let mut matrix = vec![vec![0; len]; len];
+        let mut hold = 0 - prices[0];
+        let mut free = 0;
         for i in 0..len {
-            for j in i + 1..len {
-                matrix[i][j] = prices[j] - prices[i] - fee;
-            }
+            let prev_hold = hold;
+            hold = hold.max(free - prices[i]);
+            free = free.max(prev_hold + prices[i] - fee);
         }
-        let mut profits = vec![0; len];
-        for j in 0..len {
-            profits[j] = (0..=j)
-                .map(|i| {
-                    let profit = if i == 0 { 0 } else { profits[i - 1] };
-                    profit + matrix[i][j]
-                })
-                .max()
-                .unwrap();
-        }
-        profits[len - 1]
+        free
     }
 }
 // @lc code=end
