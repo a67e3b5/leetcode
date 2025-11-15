2f3e28033343a8b70c2db14b40cb34ebda375510
a67e3b5 <142142760+a67e3b5@users.noreply.github.com>
2025-11-16 00:25:47 +0900

    fix(py): 88-2r
    Signed-off-by: a67e3b5 <142142760+a67e3b5@users.noreply.github.com>

--- python3/_88_merge_sorted_array.py
+++ python3/_88_merge_sorted_array.py
@@ -11,16 +11,16 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i = 0
-        j = 0
-        while j < n:
-            if i < m + j and nums1[i] <= nums2[j]:
-                i += 1
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
             else:
-                nums1.insert(i, nums2[j])
-                i += 1
-                j += 1
-        del nums1[m + n :]
-
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
 
 # @lc code=end
