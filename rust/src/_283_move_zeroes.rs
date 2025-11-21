6940f999d9f6c50ad30f14cd02d381b0199b34d1
a67e3b5 <142142760+a67e3b5@users.noreply.github.com>
2025-11-21 21:42:58 +0900

    perf(rs): 283-2 A
    Signed-off-by: a67e3b5 <142142760+a67e3b5@users.noreply.github.com>

--- rust/src/_283_move_zeroes.rs
+++ rust/src/_283_move_zeroes.rs
@@ -7,16 +7,23 @@
 // @lc code=start
 impl Solution {
     pub fn move_zeroes(nums: &mut Vec<i32>) {
-        let mut zeros = 0;
-        nums.retain(|n| {
-            if *n == 0 {
-                zeros += 1;
-                false
-            } else {
-                true
+        let Some(mut r) = nums.iter().position(|&num| num == 0) else {
+            return;
+        };
+        let mut w = r;
+        while r < nums.len() {
+            if nums[r] == 0 {
+                r += 1;
+                continue;
             }
-        });
-        nums.append(&mut std::iter::repeat(0).take(zeros).collect());
+            nums[w] = nums[r];
+            r += 1;
+            w += 1;
+        }
+        while w < nums.len() {
+            nums[w] = 0;
+            w += 1;
+        }
     }
 }
 // @lc code=end
