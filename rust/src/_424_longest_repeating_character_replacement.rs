fd89eac492725b86bf997dddaa6222e4bfb0c33b
a67e3b5 <142142760+a67e3b5@users.noreply.github.com>
2025-11-23 04:50:26 +0900

    fix(rs): 424-4 WA
    Signed-off-by: a67e3b5 <142142760+a67e3b5@users.noreply.github.com>

--- rust/src/_424_longest_repeating_character_replacement.rs
+++ rust/src/_424_longest_repeating_character_replacement.rs
@@ -5,40 +5,80 @@
  */
 
 // @lc code=start
-use std::cmp::max;
+use std::{cmp::max, collections::HashMap};
+
+#[derive(Debug)]
+struct Value {
+    since: usize,
+    modify: i32,
+    last_modified: usize,
+    max_len: usize,
+}
 
 impl Solution {
     pub fn character_replacement(s: String, k: i32) -> i32 {
-        let mut s: Vec<char> = s.chars().collect();
-        let max_len = Self::character_replacement_inner(&s, k);
-        s.reverse();
-        let rmax_len = Self::character_replacement_inner(&s, k);
-        max(max_len, rmax_len)
-    }
-
-    pub fn character_replacement_inner(s: &[char], k: i32) -> i32 {
-        let mut l = 0;
-        let mut r = 0;
-        let mut c = s[0];
-        let mut modify = k;
-        let mut max_len = 0;
-        while r < s.len() {
-            if s[r] == c {
-                r += 1;
-            } else if modify > 0 {
-                modify -= 1;
-                r += 1;
-            } else {
-                max_len = max(max_len, r - l);
-                l = r;
-                c = s[l];
-                modify = k;
+        let s: Vec<char> = s.chars().collect();
+        let mut map: HashMap<char, Value> = ('A'..='Z')
+            .map(|c| {
+                (
+                    c,
+                    Value {
+                        since: 0,
+                        modify: k,
+                        last_modified: 0,
+                        max_len: 0,
+                    },
+                )
+            })
+            .collect();
+        for (&c, v) in map.iter_mut() {
+            let mut r = 0;
+            while r < s.len() {
+                if s[r] == c {
+                    r += 1;
+                } else if v.modify > 0 {
+                    v.modify -= 1;
+                    v.last_modified = r;
+                    r += 1;
+                } else if k > 0 {
+                    v.max_len = max(v.max_len, r - v.since);
+                    v.since = v.last_modified + 1;
+                    v.modify = k;
+                } else {
+                    v.max_len = max(v.max_len, r - v.since);
+                    r += 1;
+                    v.since = r;
+                }
             }
+            v.max_len = max(v.max_len, r - v.since)
         }
-        max_len = max(max_len, r - l);
-        max_len as i32
+        map.values().map(|v| v.max_len).max().unwrap() as i32
     }
 }
 // @lc code=end
 
 struct Solution;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let cases = [
+            ("A", 0, 1),
+            ("Z", 9, 1),
+            ("BAAAB", 2, 5),
+            ("BABABBA", 1, 4),
+            ("AABABBA", 1, 4),
+            ("IMNJJTRMJEGMSOLSCCQICIHLQIOGBJAEHQOCRAJQMBIBATGLJDTBNCPIFRDLRIJHRABBJGQAOLIKRLHDRIGERENNMJSDSSMESSTR", 2, 6),
+        ];
+        for (s, k, a) in cases {
+            assert_eq!(
+                super::Solution::character_replacement(s.to_string(), k),
+                a,
+                "s: {s}, k: {k}"
+            );
+        }
+    }
+}
