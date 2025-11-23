0bd85cc7190ddd16d5743ec0144f5c08b02aa7c4
a67e3b5 <142142760+a67e3b5@users.noreply.github.com>
2025-11-23 12:02:54 +0900

    fix(rs): 33-6 A
    Signed-off-by: a67e3b5 <142142760+a67e3b5@users.noreply.github.com>

--- rust/src/_33_search_in_rotated_sorted_array.rs
+++ rust/src/_33_search_in_rotated_sorted_array.rs
@@ -4,27 +4,30 @@
  * [33] Search in Rotated Sorted Array
  */
 
+use std::cmp::Ordering;
+
 // @lc code=start
 impl Solution {
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-        let mut d = nums.len() - 1;
-        let mut i = 0;
-        loop {
-            if nums[i] < target {
-                i += d;
-                i %= nums.len();
-            } else if nums[i] > target {
-                if i < d {
-                    i += nums.len();
+        let mut l = 0;
+        let mut r = nums.len() - 1;
+        while l <= r {
+            let mid = (l + r) / 2;
+            if nums[mid] == target {
+                return mid as i32;
+            } else if nums[mid] >= nums[l] {
+                if nums[l] <= target && target <= nums[mid] {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
                 }
-                i -= d;
             } else {
-                return i as i32;
+                if nums[mid] <= target && target <= nums[r] {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
             }
-            if d == 0 {
-                break
-            }
-            d /= 2;
         }
         -1
     }
