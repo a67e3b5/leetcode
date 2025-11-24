02d71cc13607eade22ed5052878dde1548853f6a
a67e3b5 <142142760+a67e3b5@users.noreply.github.com>
2025-11-25 00:57:30 +0900

    fix(rs): 322-3 MLE
    Signed-off-by: a67e3b5 <142142760+a67e3b5@users.noreply.github.com>

--- rust/src/_322_coin_change.rs
+++ rust/src/_322_coin_change.rs
@@ -5,18 +5,20 @@
  */
 
 // @lc code=start
+use std::collections::VecDeque;
+
 impl Solution {
     pub fn coin_change(mut coins: Vec<i32>, amount: i32) -> i32 {
-        coins.sort_unstable();
-        let mut stack = vec![(amount, 0)];
-        while let Some((remain, num)) = stack.pop() {
+        coins.sort_unstable_by_key(|c| -c);
+        let mut queue = VecDeque::from([(amount, 0)]);
+        while let Some((remain, num)) = queue.pop_front() {
             if remain == 0 {
                 return num;
             }
             for c in &coins {
                 let remain = remain - *c;
                 if remain >= 0 {
-                    stack.push((remain, num + 1));
+                    queue.push_back((remain, num + 1));
                 }
             }
         }
