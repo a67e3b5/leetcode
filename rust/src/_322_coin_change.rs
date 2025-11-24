3c1b26e46cc8877331eb009f47ca5e0eeb3e6582
a67e3b5 <142142760+a67e3b5@users.noreply.github.com>
2025-11-25 02:27:05 +0900

    perf(rs): 322-5 A
    Signed-off-by: a67e3b5 <142142760+a67e3b5@users.noreply.github.com>

--- rust/src/_322_coin_change.rs
+++ rust/src/_322_coin_change.rs
@@ -5,26 +5,38 @@
  */
 
 // @lc code=start
-use std::collections::VecDeque;
-
 impl Solution {
-    pub fn coin_change(mut coins: Vec<i32>, amount: i32) -> i32 {
-        coins.sort_unstable_by_key(|c| -c);
-        let mut queue = VecDeque::from([(amount, 0)]);
-        while let Some((remain, num)) = queue.pop_front() {
-            if remain == 0 {
-                return num;
-            }
-            for c in &coins {
-                let remain = remain - *c;
-                if remain >= 0 {
-                    queue.push_back((remain, num + 1));
-                }
-            }
+    pub fn coin_change(coins: Vec<i32>, amount: i32) -> i32 {
+        let coins: Vec<usize> = coins.into_iter().map(|c| c as usize).collect();
+        let amount = amount as usize;
+        let mut dp = vec![i32::MAX; amount + 1];
+        dp[0] = 0;
+        for a in 1..=amount {
+            let Some(min) = coins.iter().filter(|c| a >= **c).map(|c| dp[a - *c]).min() else {
+                continue;
+            };
+            dp[a] = min.saturating_add(1);
         }
-        -1
+        if dp[amount] < i32::MAX {
+            dp[amount]
+        } else {
+            -1
+        }
     }
 }
 // @lc code=end
 
 struct Solution;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let cases = [(vec![2], 3, -1)];
+        for (coins, amount, ret) in cases {
+            assert_eq!(super::Solution::coin_change(coins, amount), ret)
+        }
+    }
+}
