09753542b6a4d8cfa173309e340854b843eb20e8
a67e3b5 <142142760+a67e3b5@users.noreply.github.com>
2025-12-23 12:48:41 +0900

    feat(ts): 15 A
    Signed-off-by: a67e3b5 <142142760+a67e3b5@users.noreply.github.com>

---
+++ typescript/_15_3_sum.ts
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=15 lang=typescript
+ *
+ * [15] 3Sum
+ */
+
+// @lc code=start
+function threeSum(nums: number[]): number[][] {
+    let res: number[][] = [];
+    nums.sort((a, b) => a - b);
+
+    for (const i of new Array(nums.length).keys()) {
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
+        let j = i + 1;
+        let k = nums.length - 1;
+        while (j < k) {
+            let total = nums[i] + nums[j] + nums[k];
+            if (total > 0) {
+                k -= 1;
+            } else if (total < 0) {
+                j += 1;
+            } else {
+                res.push([nums[i], nums[j], nums[k]]);
+                j += 1;
+                while (nums[j] === nums[j - 1] && j < k) {
+                    j += 1;
+                }
+            }
+        }
+    }
+    return res
+};
+// @lc code=end
