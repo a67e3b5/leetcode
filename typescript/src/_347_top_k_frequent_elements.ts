c078f84e7e7d29d17dff0c2934337167c714c275
a67e3b5 <142142760+a67e3b5@users.noreply.github.com>
2025-12-27 14:04:12 +0900

    feat(ts): 347-4 A
    Signed-off-by: a67e3b5 <142142760+a67e3b5@users.noreply.github.com>

--- typescript/src/_347_top_k_frequent_elements.ts
+++ typescript/src/_347_top_k_frequent_elements.ts
@@ -10,13 +10,12 @@ function topKFrequent(nums: number[], k: number): number[] {
     for (const n of nums) {
         numToFreq.set(n, (numToFreq.get(n) ?? 0) + 1);
     }
-    const freqToNum = new Map<number, number>();
+    const freqToNum = new Array<[number, number]>();
     for (const [n, f] of numToFreq.entries()) {
-        freqToNum.set(f, n);
+        freqToNum.push([f, n]);
     }
-    return [...freqToNum]
-        .sort()
-        .reverse()
+    return freqToNum
+        .sort((a, b) => b[0] - a[0])
         .slice(0, k)
         .map((v, _i, _a) => v[1])
 };
@@ -24,3 +23,7 @@ function topKFrequent(nums: number[], k: number): number[] {
 
 console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2));
 // [1, 2]: number[]
+console.log(topKFrequent([1, 1, 1, 1, 2, 2, 2, 2, 3, 3], 2));
+// [1, 2]
+console.log(topKFrequent([5, 1, -1, -8, -7, 8, -5, 0, 1, 10, 8, 0, -4, 3, -1, -1, 4, -5, 4, -3, 0, 2, 2, 2, 4, -2, -4, 8, -7, -7, 2, -8, 0, -8, 10, 8, -8, -2, -9, 4, -7, 6, 6, -1, 4, 2, 8, -3, 5, -9, -3, 6, -8, -5, 5, 10, 2, -5, -1, -5, 1, -3, 7, 0, 8, -2, -3, -1, -5, 4, 7, -9, 0, 2, 10, 4, 4, -4, -1, -1, 6, -8, -9, -1, 9, -9, 3, 5, 1, 6, -1, -2, 4, 2, 4, -6, 4, 4, 5, -5], 7));
+// [4, -1, 2, -5, -8, 8, 0]
